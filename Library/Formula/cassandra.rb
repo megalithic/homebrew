c29b89f018a19c4139c6581e7fe6244320270a3b
Seth Messer <seth.messer@gmail.com>
2014-10-26 21:15:48 -0500

    again please

--- Library/Formula/cassandra.rb
+++ Library/Formula/cassandra.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Cassandra < Formula
   homepage 'http://cassandra.apache.org'
-  url 'http://www.apache.org/dyn/closer.cgi?path=/cassandra/2.0.11/apache-cassandra-2.0.11-bin.tar.gz'
+  url 'http://mirror.cogentco.com/pub/apache/cassandra/2.0.11/apache-cassandra-2.0.11-bin.tar.gz'
   sha1 'f31d71797e1ffeeacb3c71ad35e900d11580bfc3'
 
   def install
